64f350fee9b07f40baf26f03e8b766c74fb4acd3
lifeandtimesofnoonoo <matt.goengland@gmail.com>
2018-11-24 22:49:17 +0000

    Node should now sleep between sending messages

--- Slave.ino
+++ Slave.ino
@@ -8,19 +8,20 @@ the master node via a mesh network while spending as much time as possible in a 
 #include "RF24Mesh.h"
 #include <SPI.h>
 #include <EEPROM.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
 
 RF24 radio(7, 8);                     //Sets the CE and CS pins
 RF24Network network(radio);           //Begins network using the nRF24L01+ radio
 RF24Mesh mesh(radio, network);        //Defines network as a mesh
 
-#define nodeID 2                      //Unique ID of node. In future should be set dynamically during setup
+const int nodeID = 2;                      //Unique ID of node. In future should be set dynamically during setup
 const int nodeDepth = 30;             //Depth of sensor
 uint32_t timer = 0;                   //Used for time keeping
 
 struct dataPacket                     //Stucture of data sent over RF
 {
   uint16_t temperature;                  //Measured temperature
-  //time, in some format
   uint8_t uniqueNodeID;                   //Unique ID of node - used to know where data was recorded
   uint8_t nodeDepth;                      //Depth of the sensor on this node
 };
@@ -29,22 +30,24 @@ void setup()
 {
 
   Serial.begin(115200);               //For debugging - final product unlikely to use this
-  
+
+  network.setup_watchdog(9);          //Sets up watchdog timer, 9 denotes a time period of 8 seconds, the longest possible (I'm pretty certain this means the greatest power saving)
+  ADCSRA &= ~(1 << ADEN);
+  power_adc_disable();                //Disables the ADC, should save significant power
+
   mesh.setNodeID(nodeID);             //Sets the nodeID manually
   
   Serial.println(F("Connecting to the mesh..."));
   mesh.begin(MESH_DEFAULT_CHANNEL, RF24_250KBPS, MESH_RENEWAL_TIMEOUT);     //Required for connecting to the mesh
                                                                             //data rate set as low as possible to increase antenna sensitivity
-
 }
 
 void loop() 
 {
   mesh.update();                      //Keeps mesh updated - testing required to see how this should be used when bringing node in and out of sleep
 
-  if(millis() - timer >= 5000)        //Send message every 5 seconds
-  {
-    timer = millis();
+    network.sleepNode(1,0);           //Puts node in a low power state, can be woken by an interrupt
+
     struct dataPacket toBeSent = {takeTemperature(), nodeID, nodeDepth};
 
     Serial.println("Sending data...");
@@ -61,9 +64,7 @@ void loop()
         Serial.println("Send fail, Network test OK");
       }
     }
-  }
 }
-
 
 uint32_t takeTemperature()     //Measures temperature from a sensor - currently simulates behavior
 {
