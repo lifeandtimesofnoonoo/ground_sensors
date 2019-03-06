ed80bf7c81ac9a29c1e3f9a6ccbc728eed0d5412
lifeandtimesofnoonoo <matt.goengland@gmail.com>
2019-03-06 16:32:59 +0000

    Some success, having difficulties sending data from master to slave, master gets "stuck" after having sent it

--- Master/Master.ino
+++ Master/Master.ino
@@ -1,5 +1,5 @@
 /*
-The intended purpose of this code is to allow the master node to receive data from any node in the network and record it
+  The intended purpose of this code is to allow the master node to receive data from any node in the network and record it
 */
 
 #include "RF24.h"
@@ -19,55 +19,91 @@ struct dataPacket                     //Stucture of data sent over RF
   uint8_t nodeDepth_t;                //Depth of the sensor on this node
 };
 
+byte nodeIDs[256];                     //Keeping track of node ID allocated, when 0 this means unallocated
+//Returns 0 for unallocated, the node ID if it is allocated
 void setup()
 {
   Serial.begin(115200);               //For debugging - final product unlikely to use this
-  
-  mesh.setNodeID(0);                  //Node 0 is the master node
-  Serial.println(F("Began the mesh"));
-  
-  mesh.begin(MESH_DEFAULT_CHANNEL, RF24_250KBPS, MESH_RENEWAL_TIMEOUT);     //Required for connecting to the mesh
-                                                                            //data rate set as low as possible to increase antenna sensitivity
-  Serial.println(F("Began the mesh"));                                                                          
 
+  mesh.setNodeID(0);                  //Node 0 is the master node
+  Serial.println(F("I am the master!"));
+
+  nodeIDs[0] = 1;
+  nodeIDs[1] = 1;                     //IDs 0 and 1 are reserved - assumed used by the master
+  nodeIDs[255] = 255;                 //Since 255 is default, assumed always in use
+  for (byte i = 2; i < 255; i++)
+  {
+    nodeIDs[i] = 0;                   //All node IDs are set 0 prior to use
+  }
+
+
+  mesh.begin(MESH_DEFAULT_CHANNEL, RF24_250KBPS, MESH_RENEWAL_TIMEOUT);     //Required for connecting to the mesh
+  //data rate set as low as possible to increase antenna sensitivity
+  Serial.println(F("Began the mesh"));
 }
 
 void loop()
 {
-  
+
   mesh.update();      //Call mesh.update to keep the network updated
-  
-  
+
+
+
   mesh.DHCP();        //In addition, keep the 'DHCP service' running on the master node so addresses will
-                      //be assigned to the sensor nodes
+  //be assigned to the sensor nodes
 
 
-  
-  if(network.available())      // Check for incoming data from the sensors
+
+  if (network.available())     // Check for incoming data from the sensors
   {
+    Serial.println("You've got mail");
+    delay(1);
     RF24NetworkHeader header;
     network.peek(header);
 
     dataPacket received;       //Data received from slave node
 
-    switch(header.type)        //Framework for changing behavior based on type of data
+    switch (header.type)       //Framework for changing behavior based on type of data
     {
-      case 'M':
-      network.read(header, &received, sizeof(received));
-      Serial.print("The temperature is ");
-      Serial.print(received.temperature_t);
-      Serial.print(" degrees at node ");
-      Serial.print(received.NodeID_t);
-      Serial.print(" which has a depth of ");
-      Serial.print(received.nodeDepth_t);
-      Serial.println(" cm");
-      break;
+      case 'D':                //Default message type, just data
+        network.read(header, &received, sizeof(received));
+        Serial.print("The temperature is ");
+        Serial.print(received.temperature_t);
+        Serial.print(" degrees at node ");
+        Serial.print(received.NodeID_t);
+        Serial.print(" which has a depth of ");
+        Serial.print(received.nodeDepth_t);
+        Serial.println(" cm");
+        break;
+
+      case 'C':                //New node connecting, requesting node ID
+
+        //Serial.println("I've been asked to allocate");
+        byte i = 2;
+        byte availableNode;
+        do
+        {
+          availableNode = i;
+          i = i + 1;
+
+        } while (nodeIDs[i - 1] == !0);     //Finds the smallest unallocated node ID
+        nodeIDs[i - 1] = i - 1;             //Records that the node ID has been allocated
+
+        //byte defaultNode;
+        //network.read(header, &defaultNode, sizeof(defaultNode));
+
+        mesh.write(&availableNode, 'C', sizeof(availableNode), 255);          //Reply sent to node 255, this is the default
+
+        //Serial.println("New ID sent");
+        //delay(1);
+
+        break;
 
       default:
-      network.read(header,0,0);
-      Serial.println(header.type);
-      Serial.println("Unknown header type");
-      break;
+        network.read(header, 0, 0);
+        Serial.println(header.type);
+        Serial.println("Unknown header type");
+        break;
     }
 
   }
--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -1,6 +1,6 @@
 /*
-The intended purpose of this code is to allow any slave sensor node to take sensor readings and relay them back to
-the master node via a mesh network while spending as much time as possible in a low power state.
+  The intended purpose of this code is to allow any slave sensor node to take sensor readings and relay them back to
+  the master node via a mesh network while spending as much time as possible in a low power state.
 */
 
 #include "RF24.h"
@@ -19,16 +19,17 @@ the master node via a mesh network while spending as much time as possible in a 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass our oneWire reference to Dallas Temperature. 
+// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
 RF24 radio(7, 8);                     //Sets the CE and CS pins
 RF24Network network(radio);           //Begins network using the nRF24L01+ radio
 RF24Mesh mesh(radio, network);        //Defines network as a mesh
 
-const int nodeID = 2;                 //Unique ID of node. In future should be set dynamically during setup
-const int nodeDepth = 30;             //Depth of sensor
+int nodeID = EEPROM.read(0);          //Unique ID of node. Taken from EEPROM, default is 255, available ID gets allocated by master
+const int nodeDepth = 30;             //Depth of sensor, some way of setting this automatically based on length of sensor?
 uint32_t timer = 0;                   //Used for time keeping
+bool mainFunction = false;
 
 struct dataPacket                     //Stucture of data sent over RF
 {
@@ -37,71 +38,133 @@ struct dataPacket                     //Stucture of data sent over RF
   uint8_t nodeDepth;                  //Depth of the sensor on this node
 };
 
-void setup() 
+void setup()
 {
-
   Serial.begin(115200);               //For debugging - final product unlikely to use serial
 
   sensors.begin();
+
   Serial.println(F("Sensor test:"));
   Serial.println(takeTemperature());
 
   // network.setup_watchdog(9);          //Sets up watchdog timer, 9 denotes a time period of 8 seconds, the longest possible (I'm pretty certain this means the greatest power saving)
-  
+
   //SETUP WATCHDOG TIMER
   WDTCSR = (24);          //change enable and WDE - also resets
   WDTCSR = (33);          //prescalers only - get rid of the WDE and WDCE bit
-  WDTCSR |= (1<<6);       //enable interrupt mode
+  WDTCSR |= (1 << 6);     //enable interrupt mode
 
   //Disable ADC - don't forget to flip back after waking up if using ADC in your application ADCSRA |= (1 << 7);
   ADCSRA &= ~(1 << 7);
-  
+
   //ENABLE SLEEP - this enables the sleep mode
   SMCR |= (1 << 2);     //power down mode
   SMCR |= 1;            //enable sleep
 
   mesh.setNodeID(nodeID);             //Sets the nodeID manually
+
   Serial.println(F("I am a slave!"));
-  
+
   mesh.begin(MESH_DEFAULT_CHANNEL, RF24_250KBPS, MESH_RENEWAL_TIMEOUT);     //Required for connecting to the mesh
-                                                                            //data rate set as low as possible to increase antenna sensitivity
+  //data rate set as low as possible to increase antenna sensitivity
   Serial.println(F("Connected to the mesh"));
+
+  mesh.write(&nodeID, 'C', sizeof(nodeID));   //Needs some form of error checking
+//  while (mesh.write(&nodeID, 'C', sizeof(nodeID)) == false)   //No useful data sent, header used to request node ID
+//  {
+//    if (!mesh.checkConnection())                        //Refresh the network address
+//    {
+//      Serial.println("Renewing Address");
+//      mesh.renewAddress();                              //Re-establishes connection to mesh
+//    }
+//    else
+//    {
+//      Serial.println("Send fail, Network test OK");
+//    }
+//  }
+
+//  Serial.println("Waiting to be allocated nodeID");
+//  delay(1);
+//  RF24NetworkHeader header;
+//
+//
+//  while (network.read(header, &nodeID, sizeof(nodeID)) == false)
+//  {
+//  }
+//
+//  delay(1);
+//  Serial.println("My new ID is:");
+//  Serial.println(nodeID);
+//  delay(1);
+//
+//  mesh.releaseAddress();
+//  mesh.setNodeID(nodeID);
+//  mesh.begin(MESH_DEFAULT_CHANNEL, RF24_250KBPS, MESH_RENEWAL_TIMEOUT);
+
 }
 
-void loop() 
+void loop()
 {
   mesh.update();                      //Keeps mesh updated - testing required to see how this should be used when bringing node in and out of sleep
 
-    
-   // network.sleepNode(1,0);           //Puts node in a low power state, can be woken by an interrupt - part of RF24Network, doesn't seem to be working
+  if (mainFunction == true)
+  {
+
     goToSleep(1);
-    
+
     struct dataPacket toBeSent = {takeTemperature(), nodeID, nodeDepth};
 
     Serial.println("Sending data...");
 
-    if(!mesh.write(&toBeSent, 'M', sizeof(toBeSent)))   //If write fails, check conectivity
+    if (!mesh.write(&toBeSent, 'D', sizeof(toBeSent)))  //If write fails, check conectivity
     {
-      if(!mesh.checkConnection())                         //Refresh the network address 
+      if (!mesh.checkConnection())                        //Refresh the network address
       {
         Serial.println("Renewing Address");
         mesh.renewAddress();                              //Re-establishes connection to mesh
-      } 
-      else 
+      }
+      else
       {
         Serial.println("Send fail, Network test OK");
       }
     }
+  }
+
+  if(network.available())       //Framework for receiving messages
+  {
+    RF24NetworkHeader header;
+    network.peek(header);
+    switch(header.type)
+    {
+      case 'C':                 //As with the master, messages are split into types specified in their headers
+      network.read(header, &nodeID, sizeof(nodeID));
+      mesh.releaseAddress();
+      mesh.setNodeID(nodeID);               //Testing required to figure out what needs to happen to connect a node with a new node ID
+      //mesh.begin(MESH_DEFAULT_CHANNEL, RF24_250KBPS, MESH_RENEWAL_TIMEOUT);
+      mesh.renewAddress();
+      delay(1);
+      Serial.println("My new ID is:");
+      Serial.println(nodeID);
+      delay(1);
+      mainFunction = true;    //Slave told to start taking and sending measurements after receiving new ID
+      break;
+
+      default:
+      network.read(header,0,0);
+      break;
+    }
+  }
+  
 }
 
-void goToSleep(int cycles)      //Cycles are periods of 8 seconds
+void goToSleep(int cycles)
 {
   Serial.println("Sleeping...");
   Serial.println("");
   delay(2);
   radio.powerDown();      //Powers down radio module - testing may be required as to how this affects mesh functionality - should also be able to reduce power consumption without completely powering down?
 
-  for (cycles; cycles >= 0; cycles--)          
+  for (cycles; cycles >= 0; cycles--)
   {
     //BOD DISABLE - this must be called right before the __asm__ sleep instruction
     MCUCR |= (3 << 5);                          //set both BODS and BODSE at the same time
@@ -116,5 +179,5 @@ void goToSleep(int cycles)      //Cycles are periods of 8 seconds
 float takeTemperature()     //Measures temperature from a sensor
 {
   sensors.requestTemperatures();      //Send the command to get temperatures
-  return(sensors.getTempCByIndex(0));
+  return (sensors.getTempCByIndex(0));
 }
