9a663814dab3e42b6c1b27135390d7beb4789fff
lifeandtimesofnoonoo <matt.goengland@gmail.com>
2019-03-07 22:38:51 +0000

    Hopefully fixed library conflicts - needs testing

--- Master/Master.ino
+++ Master/Master.ino
@@ -1,12 +1,20 @@
 /*
-  The intended purpose of this code is to allow the master node to receive data from any node in the network and record it
+ * To fix library conflicts, in Sd2PinMap.h, comment lines:
+ * 449
+ * 450
+ * 457
+ * 469
+ * 471 - 491
+ * 
+ * Also need to uncomment define SOFTSPI in RF24_config.h
 */
-
+#include <SPI.h>
+#include <SD.h>
 #include "RF24.h"
 #include "RF24Network.h"
 #include "RF24Mesh.h"
-#include <SPI.h>
 #include <EEPROM.h>
+#include "DigitalIO.h"
 
 RF24 radio(7, 8);                     //Sets the CE and CS pins
 RF24Network network(radio);           //Begins network using the nRF24L01+ radio
@@ -19,84 +27,59 @@ struct dataPacket                     //Stucture of data sent over RF
   uint8_t nodeDepth_t;                //Depth of the sensor on this node
 };
 
-byte nodeIDs[256];                     //Keeping track of node ID allocated, when 0 this means unallocated
-//Returns 0 for unallocated, the node ID if it is allocated
-void setup()
-{
-  Serial.begin(115200);               //For debugging - final product unlikely to use this
 
-  mesh.setNodeID(0);                  //Node 0 is the master node
-  Serial.println(F("I am the master!"));
+const int chipSelect = 10;
+File file;
+String dataString = "";
 
-  nodeIDs[0] = 1;
-  nodeIDs[1] = 1;                     //IDs 0 and 1 are reserved - assumed used by the master
-  nodeIDs[255] = 255;                 //Since 255 is default, assumed always in use
-  for (byte i = 2; i < 255; i++)
-  {
-    nodeIDs[i] = 0;                   //All node IDs are set 0 prior to use
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+  }
+  else {
+    Serial.println("card initialized.");
   }
 
 
+  mesh.setNodeID(0);                  //Node 0 is the master node
+
   mesh.begin(MESH_DEFAULT_CHANNEL, RF24_250KBPS, MESH_RENEWAL_TIMEOUT);     //Required for connecting to the mesh
-  //data rate set as low as possible to increase antenna sensitivity
-  Serial.println(F("Began the mesh"));
+  Serial.println(F("Connected"));
+
 }
 
-void loop()
-{
-
+void loop() {
   mesh.update();      //Call mesh.update to keep the network updated
-
-
 
   mesh.DHCP();        //In addition, keep the 'DHCP service' running on the master node so addresses will
   //be assigned to the sensor nodes
-
-
-
   if (network.available())     // Check for incoming data from the sensors
   {
-    Serial.println("You've got mail");
-    delay(1);
+    Serial.println("HELLO");
     RF24NetworkHeader header;
     network.peek(header);
 
     dataPacket received;       //Data received from slave node
-
+    Serial.println("GOT MAIL");
     switch (header.type)       //Framework for changing behavior based on type of data
     {
-      case 'D':                //Default message type, just data
+      case 'M':
         network.read(header, &received, sizeof(received));
-        Serial.print("The temperature is ");
-        Serial.print(received.temperature_t);
-        Serial.print(" degrees at node ");
-        Serial.print(received.NodeID_t);
-        Serial.print(" which has a depth of ");
-        Serial.print(received.nodeDepth_t);
-        Serial.println(" cm");
-        break;
-
-      case 'C':                //New node connecting, requesting node ID
-
-        //Serial.println("I've been asked to allocate");
-        byte i = 2;
-        byte availableNode;
-        do
-        {
-          availableNode = i;
-          i = i + 1;
-
-        } while (nodeIDs[i - 1] == !0);     //Finds the smallest unallocated node ID
-        nodeIDs[i - 1] = i - 1;             //Records that the node ID has been allocated
-
-        //byte defaultNode;
-        //network.read(header, &defaultNode, sizeof(defaultNode));
-
-        mesh.write(&availableNode, 'C', sizeof(availableNode), 255);          //Reply sent to node 255, this is the default
-
-        //Serial.println("New ID sent");
-        //delay(1);
-
+        Serial.println("WOW NETWORK");
+        // make a string for assembling the data to log:
+        // read temperature sensors and append to the string:
+        dataString += String(received.NodeID_t);
+        dataString += ", ";
+        dataString += String(received.temperature_t);
+        dataString += ", ";
+        dataString += String(received.nodeDepth_t);
+        Serial.println(dataString);
         break;
 
       default:
@@ -107,5 +90,21 @@ void loop()
     }
 
   }
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  if (!SD.begin(chipSelect)) {
+    File dataFile = SD.open("datalog.txt", FILE_WRITE);
 
+    // if the file is available, write to it:
+    if (dataFile && dataString != "") {
+      dataFile.println(dataString);
+      dataFile.close();
+      // print to the serial port too:
+      Serial.println(dataString);
+    }
+    // if the file isn't open, pop up an error:
+    else {
+      Serial.println("error opening datalog.txt");
+    }
+  }
 }
