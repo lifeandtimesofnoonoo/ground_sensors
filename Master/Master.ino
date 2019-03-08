82307c86f9ffbd6c7f915c01a1c4ac6441264fa4
lifeandtimesofnoonoo <matt.goengland@gmail.com>
2019-03-08 18:27:43 +0000

    Cleaned up code a bunch

--- Master/Master.ino
+++ Master/Master.ino
@@ -7,9 +7,8 @@
 #include "RF24Network.h"
 #include "RF24Mesh.h"
 #include <EEPROM.h>
-//#include "DigitalIO.h"
 
-RF24 radio(7, 8);                     //Sets the CE and CS pins
+RF24 radio(7, 8);                     //Sets the CE and CS pins for the radio
 RF24Network network(radio);           //Begins network using the nRF24L01+ radio
 RF24Mesh mesh(radio, network);        //Defines network as a mesh
 
@@ -20,73 +19,63 @@ struct dataPacket                     //Stucture of data sent over RF
   uint8_t nodeDepth_t;                //Depth of the sensor on this node
 };
 
-
-const int chipSelect = 10;
+const int chipSelect = 10;            //Sets the CS pin for the sd card reader
 File file;
 String dataString = "";
 
-void setup() {
-  // Open serial communications and wait for port to open:
+void setup()
+{
   Serial.begin(115200);
   Serial.print("Initializing SD card...");
 
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
+  if (!SD.begin(chipSelect))          //See if the card is present and can be initialized:
+  {
     Serial.println("Card failed, or not present");
-    // don't do anything more:
   }
   else {
     Serial.println("card initialized.");
   }
 
-
   mesh.setNodeID(0);                  //Node 0 is the master node
-
   mesh.begin(MESH_DEFAULT_CHANNEL, RF24_250KBPS, MESH_RENEWAL_TIMEOUT);     //Required for connecting to the mesh
-  Serial.println(F("Connected"));
-
+                                                                            //data rate set as low as possible to increase antenna sensitivity
+  Serial.println(F("Began the mesh"));
 }
 
-void loop() {
+void loop()
+{
   mesh.update();      //Call mesh.update to keep the network updated
-
   mesh.DHCP();        //In addition, keep the 'DHCP service' running on the master node so addresses will
-  //be assigned to the sensor nodes
+                      //be assigned to the sensor nodes
   if (network.available())     // Check for incoming data from the sensors
   {
-    Serial.println("HELLO");
-    RF24NetworkHeader header;
-    network.peek(header);
-
+    Serial.println("You've got mail!");
+    RF24NetworkHeader header;  //Initialises a header to determine message content
+    network.peek(header);      //Check content of next message
     dataPacket received;       //Data received from slave node
-    Serial.println("GOT MAIL");
     switch (header.type)       //Framework for changing behavior based on type of data
     {
       case 'M':
         network.read(header, &received, sizeof(received));
-        Serial.println("WOW NETWORK");
-        // make a string for assembling the data to log:
-        // read temperature sensors and append to the string:
+                                                        
+        dataString = "";                                 //Make a string for assembling the data to log              
         dataString += String(received.NodeID_t);
         dataString += ", ";
         dataString += String(received.temperature_t);
         dataString += ", ";
         dataString += String(received.nodeDepth_t);
-        //Serial.println(dataString);
-        File dataFile = SD.open("datalog.txt", FILE_WRITE);
-
-        // if the file is available, write to it:
-        if (dataFile) {
+        File dataFile = SD.open("datalog.txt", FILE_WRITE);   //Open file, prepare to write to it
+     
+        if (dataFile)                                   //If the file is available, write to it
+        {
           dataFile.println(dataString);
-          dataFile.close();
-          // print to the serial port too:
-          Serial.println(dataString);
+          dataFile.close();                             //Is it required to close the file after use if we're only ever writing to one file?       
+          Serial.println(dataString);                   //Print to the serial port too
         }
-        // if the file isn't open, pop up an error:
-        else {
+        else 
+        {
           Serial.println("error opening datalog.txt");
-        }
-        dataString = "0";
+        }                              
         break;
 
       default:
